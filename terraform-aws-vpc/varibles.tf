8c295c1a315da765d03001495ede448db06717e9
shirisha hosur <shirisha.h916@gmail.com>
2024-05-23 20:58:39 +0530

    vpc

---
+++ terraform-aws-vpc/varibles.tf
@@ -0,0 +1,109 @@
+variable "project_name" {
+    type = string
+}
+variable "environment" {
+    type = string
+    default = "dev"  
+}
+variable "common_tags" {
+    type = map
+  
+}
+
+## VPC ###
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16"
+  
+}
+
+variable "enable_dns_hostnames" {
+    type = bool
+    default = true  
+}
+variable "vpc_tags" {
+    type = map
+    default = {}
+  
+}
+###IGW###
+variable "igw_tags" {
+    type = map
+    default = {}
+}
+### Public Subnet###
+variable "public_subnet_cidrs" {
+    type = list
+    validation {
+        condition = length(var.public_subnet_cidrs) == 2
+        error_message = "Please provide 2 valid public subnet cidrs"
+    }
+  
+}
+variable "public_subnet_cidr_tags" {
+    type = map
+    default = {}
+}
+### Private Subnet###
+variable "private_subnet_cidrs" {
+    type = list
+    validation {
+        condition = length(var.private_subnet_cidrs) == 2
+        error_message = "Please provide 2 valid public subnet cidrs"
+    }
+  
+}
+variable "private_subnet_cidr_tags" {
+    type = map
+    default = {}
+}
+### Database Subnet###
+variable "database_subnet_cidrs" {
+    type = list
+    validation {
+        condition = length(var.database_subnet_cidrs) == 2
+        error_message = "Please provide 2 valid public subnet cidrs"
+    }
+  
+}
+variable "database_subnet_cidr_tags" {
+    type = map
+    default = {}
+}
+## NAt gatway##
+variable "aws_nat_gateway_tags" {
+    type = map
+    default = {}
+}
+## Public route table
+variable "public_route_table_tags" {
+    type = map
+    default = {}
+}
+
+##private route table
+variable "private_route_table_tags" {
+    type = map
+    default = {}
+}
+
+## database route table
+variable "database_route_table_tags" {
+    type = map
+    default = {}
+}
+### Perring ###
+variable "is_peering_required" {
+    type = bool
+    default = false
+}
+variable "acceptor_vpc_id" {
+    type = string
+    default = ""
+  
+}
+
+variable "vpc_peering_tags" {
+  type = map
+  default = {}
+}
